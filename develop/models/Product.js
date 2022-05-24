33832c06d4a8ab6f3cbee8368542d4d9341447d7
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-24 14:43:52 -0400

    product model

--- develop/models/Product.js
+++ develop/models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING, 
+      alllowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
+    }, 
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+    }, 
+    category_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'category', 
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
